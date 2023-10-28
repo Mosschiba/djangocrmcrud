3c0805a415441c4c4784c247b7cb16f91c2c438d
unknown <mousslemchibane@gmail.com>
2023-10-28 14:30:02 +0400

    dit

--- crmapp/urls.py
+++ crmapp/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     # path('login/', views.login_user, name='login'),
     path('logout/', views.logout_user, name='logout'),
     path('register/', views.register_user, name='register'),
+    path('record/<int:pk>', views.customer_record, name='record'),
+    path('edit_record/<int:pk>', views.edit_record, name='edit'),
 
 ]
--- crmapp/views.py
+++ crmapp/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from .forms import SignUpForm
+from .models import Record
 
 
 def home(request):
+    records = Record.objects.all()
     # check if loggin in
     if request.method == 'POST':
         username = request.POST['username']
@@ -20,7 +22,7 @@ def home(request):
                 request, 'there was some thing wrong, Please try again')
             return redirect('home')
     else:
-        return render(request, 'home.html', {})
+        return render(request, 'home.html', {'records': records})
 
 
 def logout_user(request):
@@ -45,3 +47,19 @@ def register_user(request):
         form = SignUpForm()
         return render(request, 'register.html', {'form': form})
     return render(request, 'register.html', {'form': form})
+
+
+def customer_record(request, pk):
+    if request.user.is_authenticated:
+        # lookup for that record
+        customer_record = Record.objects.get(id=pk)
+        return render(request, 'record.html', {'customer_record': customer_record})
+    else:
+        messages.success(
+            request, 'You need to loging to view the cutomer details')
+        return redirect('home')
+
+
+def edit_record(request):
+
+    pass
