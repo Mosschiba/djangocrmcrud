7deaa158c3463d67d252de1406c1d4d45e853446
unknown <mousslemchibane@gmail.com>
2023-10-30 17:03:29 +0400

    update and edit

--- crmapp/forms.py
+++ crmapp/forms.py
@@ -40,21 +40,28 @@ class SignUpForm(UserCreationForm):
 
 class AddRecordForm(forms.ModelForm):
     first_name = forms.CharField(required=True, widget=forms.widgets.TextInput(
-        attrs={"placeholder": "First Name", "class": "form-control"}))
+        attrs={"placeholder": "First Name", "class": "form-control"}), label="")
+
     last_name = forms.CharField(required=True, widget=forms.widgets.TextInput(
-        attrs={"placeholder": "Last Name", "class": "form-control"}))
+        attrs={"placeholder": "Last Name", "class": "form-control"}), label="")
+
     email = forms.CharField(required=True, widget=forms.widgets.EmailInput(
-        attrs={"placeholder": "Email", "class": "form-control"}))
+        attrs={"placeholder": "Email", "class": "form-control"}), label="")
+
     phone = forms.CharField(required=True,  widget=forms.widgets.NumberInput(
-        attrs={"placeholder": "Phone", "class": "form-control"}))
+        attrs={"placeholder": "Phone", "class": "form-control"}), label="")
+
     address = forms.CharField(required=True, widget=forms.widgets.TextInput(
-        attrs={"placeholder": "Address", "class": "form-control"}))
+        attrs={"placeholder": "Address", "class": "form-control"}), label="")
+
     city = forms.CharField(required=True, widget=forms.widgets.TextInput(
-        attrs={"placeholder": "City", "class": "form-control"}))
+        attrs={"placeholder": "City", "class": "form-control"}), label="")
+
     state = forms.CharField(required=True, widget=forms.widgets.TextInput(
-        attrs={"placeholder": "State", "class": "form-control"}))
+        attrs={"placeholder": "State", "class": "form-control"}), label="")
+
     zipcode = forms.CharField(required=True, widget=forms.widgets.TextInput(
-        attrs={"placeholder": "Zipcode", "class": "form-control"}))
+        attrs={"placeholder": "Zipcode", "class": "form-control"}), label="")
 
     class Meta:
         model = Record
